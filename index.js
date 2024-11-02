258e0524018c7eaff5f47e74a856b2e875a0ddf1
Divyapriya-K <diyapriya0428@gmail.com>
2024-11-02 21:52:36 +0530

    Completed

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors({
+    origin:"*",
+    credentials:true,
+    methods:["GET","PUT","POST","DELETE"]
+}));
+
+app.get("/",(req,res)=>{
+    res.status(200).send("welcome to out API(PASSWORD-RESET)")
+})
+
+app.use(express.json())
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log("server started and running on the port");
+    
+})

f4ad8cdafacf0ea6b4d7b09c2703a921132f16ba
Divyapriya-K <diyapriya0428@gmail.com>
2025-03-07 08:39:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors({
 }));
 
 app.get("/",(req,res)=>{
-    res.status(200).send("welcome to our API(PASSWORD-RESET)")
+    res.status(200).send("welcome to our backen API")
 })
 
 app.use(express.json())
